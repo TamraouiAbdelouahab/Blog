3716e64a0b9fa5c671daba548831802cc3004c7e
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2025-02-27 16:54:38 +0000

    Ajouter laravel helloWorld

---
+++ app/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="margin:0;padding:0;">
+    <div style="height: 100vh; display: flex; align-items: center; justify-content: center;">
+        <h1>Hello World</h1>
+    </div>
+</body>
+</html>
